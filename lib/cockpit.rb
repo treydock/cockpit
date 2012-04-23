76564a8fb357995dc58ea43177642f554357516a
Trey Dockendorf <treydock@gmail.com>
2012-04-22 19:17:15 -0500

    - Working to make Cockpit::Settings accessible in initializers

--- lib/cockpit.rb
+++ lib/cockpit.rb
@@ -1,17 +1,30 @@
 require 'rubygems'
 require 'defined-by'
 
+require 'cockpit/core/include'
+#require 'cockpit/core/scope'
+#require 'cockpit/core/store'
+#require 'cockpit/core/helpers'
+#require 'cockpit/core/global'
+#require 'cockpit/stores/active_record'
+require 'cockpit/core/settings'
+
 module Cockpit
+
   autoload :Scope,        'cockpit/core/scope'
   autoload :Settings,     'cockpit/core/settings'
   autoload :Store,        'cockpit/core/store'
   autoload :ViewHelpers,  'cockpit/core/helpers'
+
+  module Rails
+    class Railtie < ::Rails::Railtie
+      initializer 'cockpit.ar_extensions' do |app|
+        ActiveRecord::Base.send(:include, Cockpit) if defined?(ActiveRecord::Base)
+      end
+    end
+  end
 end
-
-require 'cockpit/core/include'
-
-ActiveRecord::Base.send(:include, Cockpit) if defined?(ActiveRecord::Base)
-
+  
 def Settings(*args, &block)
   Cockpit::Settings.define!(*args, &block)
 end
